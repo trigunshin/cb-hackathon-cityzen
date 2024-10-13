af65a1734d17f7845346c28cf109b171ef674fd6
Nick Turner <github@turneron.com>
2024-10-12 21:10:13 -0700

    Etl refactor (#3)
    * refactored etl
    
    * etl refactor working

--- etl/models/newsapi_data.py
+++ etl/models/newsapi_data.py
@@ -1,16 +1,18 @@
-from sqlalchemy import Column, Integer, String, DateTime, func, JSON
+from sqlalchemy import Column, Integer, String, DateTime, func, JSON, Text
 from sqlalchemy.orm import DeclarativeBase
 
 
 class Base(DeclarativeBase):
     pass
 
-class RawTranscriptData(Base): 
-    __tablename__ = 'raw_transcript_data'
+class RawNewsApiData(Base): 
+    __tablename__ = 'raw_newsapi_data'
     id = Column(Integer, primary_key=True, autoincrement=True) 
-    json_metadata = Column(JSON) 
+    json_response = Column(JSON) 
     transcript_text = Column(String)
+    title = Column(String)
+    body = Column(Text)
     source = Column(String)
     created_at = Column(DateTime, default=func.now())
 
-RawTranscriptDataBase = Base
+RawNewsApiDataBase = Base
