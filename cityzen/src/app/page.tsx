5c1575d2886120ffa9efd0373844aa5a1bf8cf5a
Emilia Doda <edoda@usc.edu>
2024-10-13 07:48:32 -0700

    fix

--- cityzen/src/app/page.tsx
+++ cityzen/src/app/page.tsx
@@ -7,7 +7,18 @@ import React, { useState } from "react";
 import FlexibleChipStack from "./components/flexibleChips";
 import ModernButton from "./components/modernButton";
 import "./globals.css";
-import EventCarousel from "./components/eventCarousel";
+import EventCarousel from "./components/EventCarousel";
+
+const METRALL_INFO = {
+  left: {
+    title: "Simplifying civic engagement in LA. Your city's pulse, at your fingertips.",
+    description: "Metrall empowers residents of Los Angeles by offering easy access to the latest local news, events, and civic activities. With AI-driven insights, Metrall keeps you informed on important community matters, from city council meetings to public events, all in one intuitive platform. Stay connected to the pulse of your neighborhood and make informed decisions about your city."
+  },
+  right: {
+    title: "Empowering informed communities through AI-driven insights.",
+    description: "Metrall is more than just a news aggregator; it brings together real-time data from various sources, including city governance updates, local events, and social causes, offering personalized information based on your interests and location. Whether it's attending a neighborhood council meeting or learning about a new policy affecting your area, Metrall makes civic participation accessible for everyone."
+  }
+};
 
 export default function Home() {
   const [inputValue, setInputValue] = useState("");
@@ -35,7 +46,10 @@ export default function Home() {
 
   return (
     <>
-      <Box
+      <Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', color: "#868686"}}>
+    <Box
+        flex={1}
+        bgcolor={'#eeeeee'}
         sx={{
           alignContent: 'center'
         }}
@@ -75,8 +89,6 @@ export default function Home() {
                 },
               }}
             />
-
-
 
             <Box width="100%">
               <FlexibleChipStack />
@@ -133,7 +145,7 @@ export default function Home() {
                 fontSize: 'clamp(0.875rem, 2vw, 1rem)',
               }}
             >
-              {section.content}
+              {section.description}
             </Typography>
           </Paper>
         </Grid>
